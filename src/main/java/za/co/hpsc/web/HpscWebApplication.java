c39b7d020871230f92a2b828563cb8804603cfc0
Leoni Lubbinge <tahoni@gmail.com>
2026-01-03 10:09:52 +0200

    Initial Spring Boot commit

---
+++ src/main/java/za/co/hpsc/web/HpscWebApplication.java
@@ -0,0 +1,13 @@
+package za.co.hpsc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpscWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpscWebApplication.class, args);
+	}
+
+}
