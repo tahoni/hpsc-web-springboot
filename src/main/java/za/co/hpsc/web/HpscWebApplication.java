f99557427717df9feff4dc00f2647816bf784b3d
Leoni Lubbinge <tahoni@gmail.com>
2026-02-10 14:19:07 +0200

    Update version to 3.0.1, enhance exception handling, and refine API documentation

--- src/main/java/za/co/hpsc/web/HpscWebApplication.java
+++ src/main/java/za/co/hpsc/web/HpscWebApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "hpsc-web", version = "3.0.0",
+@OpenAPIDefinition(info = @Info(title = "hpsc-web", version = "3.0.1",
         description = "HPSC website backend."))
 @SpringBootApplication
 public class HpscWebApplication {
--- src/main/java/za/co/hpsc/web/configs/ControllerAdvice.java
+++ src/main/java/za/co/hpsc/web/configs/ControllerAdvice.java
@@ -1,7 +1,6 @@
 package za.co.hpsc.web.configs;
 
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
-import com.fasterxml.jackson.dataformat.csv.CsvReadException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -41,7 +40,7 @@ public class ControllerAdvice {
      * with HTTP status 500 (Internal Server Error).
      */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({Exception.class, RuntimeException.class})
     public ResponseEntity<ControllerResponse> handleGeneralException(Exception ex,
                                                                      WebRequest request) {
         ControllerResponse errorResponse = new ControllerResponse(LocalDateTime.now(), ex.getMessage(),
@@ -51,80 +50,19 @@ public class ControllerAdvice {
 
 
     /**
-     * Handles exceptions of type {@link RuntimeException} that occur during the
-     * processing of requests.
-     * Constructs a response entity containing an error message, a timestamp,
-     * and additional error details to provide feedback to the client.
-     *
-     * @param ex      the runtime exception that was thrown.
-     * @param request the current web request context.
-     * @return a {@link ResponseEntity} containing a structured error response
-     * with HTTP status 500 (Internal Server Error).
-     */
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ControllerResponse> handleRuntimeException(RuntimeException ex,
-                                                                     WebRequest request) {
-        ControllerResponse errorResponse = new ControllerResponse(LocalDateTime.now(), ex.getMessage(),
-                "Internal Server Error");
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * Handles exceptions of type {@link IllegalArgumentException} that occur during
-     * the processing of requests.
+     * Handles validation exceptions that occur during the processing of requests.
      * Constructs a response entity containing an error message, a timestamp,
      * and additional error details to notify the client of a bad request.
      *
-     * @param ex      the {@link IllegalArgumentException} that was thrown.
+     * @param ex      the exception that was thrown.
      * @param request the current web request context.
      * @return a {@link ResponseEntity} containing a structured error response
      * with HTTP status 400 (Bad Request).
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
+    @ExceptionHandler({IllegalArgumentException.class, MismatchedInputException.class})
     public ResponseEntity<ControllerResponse> handleIllegalArgumentException(IllegalArgumentException ex,
                                                                              WebRequest request) {
-        ControllerResponse errorResponse = new ControllerResponse(LocalDateTime.now(), ex.getMessage(),
-                "Bad Request");
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    /**
-     * Handles exceptions of type {@link MismatchedInputException} that occur during
-     * the processing of requests. Constructs a response entity containing an error
-     * message, a timestamp, and details about the mismatched input, providing feedback
-     * to the client about the bad request.
-     *
-     * @param ex      the {@link MismatchedInputException}, that was thrown.
-     * @param request the current web request context.
-     * @return a {@link ResponseEntity} containing a structured error response
-     * with HTTP status 400 (Bad Request).
-     */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MismatchedInputException.class)
-    public ResponseEntity<ControllerResponse> handleMismatchedInputException(MismatchedInputException ex,
-                                                                             WebRequest request) {
-        ControllerResponse errorResponse = new ControllerResponse(LocalDateTime.now(), ex.getMessage(),
-                "Bad Request");
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    /**
-     * Handles exceptions of type {@link CsvReadException} that occur during
-     * the processing of requests. Constructs a response entity containing an
-     * error message, a timestamp, and additional error details to notify the
-     * client of a bad request.
-     *
-     * @param ex      the {@link CsvReadException} that was thrown.
-     * @param request the current web request context.
-     * @return a {@link ResponseEntity} containing a structured error response
-     * with HTTP status 400 (Bad Request).
-     */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(CsvReadException.class)
-    public ResponseEntity<ControllerResponse> handleCsvReadException(CsvReadException ex,
-                                                                     WebRequest request) {
         ControllerResponse errorResponse = new ControllerResponse(LocalDateTime.now(), ex.getMessage(),
                 "Bad Request");
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
